c71d042793838e5328edfc3055d3a370f4fef812
Patrixion <patrixion164@gmail.com>
2022-05-22 19:03:33 +0200

    preparing for JS and added some animations

---
+++ index.js
@@ -0,0 +1,5 @@
+// 1. bierzemy arrowy. onClick
+// 2. sprawdzamy czy są aktywne
+// 3. Jak jest aktywny to zmieniamy jego klasę po kliknięciu
+//  na nieaktywną plus zmieniamy klasę card w box_card na nieaktywną 
+//  a nieaktywną w aktywną 
