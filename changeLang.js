8d9c2b50edaa89de396fbefedec5bd6706d29b70
TomaszFeszter <tomek.feszter.grupa@gmail.com>
2022-06-02 04:13:57 +0200

    dropdown fix, all js

--- changeLang.js
+++ changeLang.js
@@ -1,10 +1,20 @@
 const dropdownEl = Array.from(document.querySelectorAll('.dropdown__element'));
+const dropdownBtn = document.querySelector('.dropdown--btn');
+const dropdownList = document.querySelector('.dropdown');
+const dropdownArrow = document.querySelector('.arrow-down');
+
+dropdownBtn.addEventListener('click', function () {
+    dropdownList.classList.toggle('dropdown--active');
+    dropdownArrow.classList.toggle('arrow-rotate');
+})
 
 dropdownEl.forEach(lang => {
     lang.addEventListener('click', function () {
-        const currentLang = document.querySelector('.dropdown--hover');
+        const currentLang = document.querySelector('.dropdown--btn');
         const currentLangName = currentLang.outerText;
         currentLang.innerHTML = lang.outerText;
         lang.innerHTML = currentLangName;
+        dropdownList.classList.toggle('dropdown--active');
+        dropdownArrow.classList.toggle('arrow-rotate');
     })
 })
